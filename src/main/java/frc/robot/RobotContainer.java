72cc5abd7c3102112ac0579abf986133db71ed21
slashdasher <yoofiae@gmail.com>
2025-01-25 09:48:39 -0500

    created new paths for the auto routine

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -52,7 +52,7 @@ public class RobotContainer {
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   private final Telemetry logger = new Telemetry(MaxSpeed);
-  Vision m_vision = new Vision();
+  // Vision m_vision = new Vision();
   intake m_Intake = new intake();
   Elevator m_elevator = new Elevator();
 
@@ -112,7 +112,7 @@ public class RobotContainer {
 
     // Configure the trigger bindings
     configureBindings();
-    SmartDashboard.putData(m_vision);
+    // SmartDashboard.putData(m_vision);
     SmartDashboard.putData(m_elevator);
   }
 
@@ -150,6 +150,6 @@ public class RobotContainer {
     // This method loads the auto when it is called, however, it is recommended
     // to first load your paths/autos when code starts, then return the
     // pre-loaded auto/path
-    return new PathPlannerAuto("Forward Backward Left Right");
+    return new PathPlannerAuto("Forward");
   }
 }
