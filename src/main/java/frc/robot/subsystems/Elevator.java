1f2b6102590bcbf6c5e0764b71a60ba3c15efd68
NotAVeryHappyPerson <rithviknadendla@gmail.com>
2025-02-10 17:19:15 -0500

    Changes from last Saturday

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,46 +4,97 @@
 
 package frc.robot.subsystems;
 
+import java.util.HashMap;
+
+import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.sim.CANrangeSimState;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
   /** Creates a new Elevator. */
-  public final TalonFX elevatorMotor;
+  public final TalonFX leftElevatorMotor, rightElevatorMotor;
+  public CANrange canRange;
+  public PIDController PID;
+  private double pidCalc = 0;
+  private double position = 0.37;
+  private HashMap<String, Double> elevatorPosition;
 
   public Elevator() {
-    elevatorMotor = new TalonFX(9);
+    PID = new PIDController(0.7, 0, 0);//ONLY SET THE P VALUE
+
+
+    leftElevatorMotor = new TalonFX(9);
+    rightElevatorMotor = new TalonFX(10);
+
+    leftElevatorMotor.setNeutralMode(NeutralModeValue.Brake);
+    rightElevatorMotor.setNeutralMode(NeutralModeValue.Brake);
+
+    canRange = new CANrange(2);
+
+    elevatorPosition = new HashMap<>();
+    elevatorPosition.put("Default", 0.37);
+    elevatorPosition.put("L2", 0.420);
+    elevatorPosition.put("L3", 0.67);
   }
 
-  public void runElevatorMotor(double speed) {
-    elevatorMotor.set(speed);
+
+  public void runElevatorMotors(double speed) {
+    leftElevatorMotor.set(speed);
+    rightElevatorMotor.set(speed);
   }
 
-  public void stopElevatorMotor() {
-    elevatorMotor.stopMotor();
+  public void setMotorToPIDCalc() {
+    pidCalc = PID.calculate(canRange.getDistance().getValueAsDouble(), position);
+    runElevatorMotors(pidCalc);
+  }
+
+  public void stopElevatorMotors() {
+    leftElevatorMotor.stopMotor();
+    rightElevatorMotor.stopMotor();
+  }
+
+  public void setElevatorState(String targetPosition){
+    position = elevatorPosition.get(targetPosition);
+  }
+
+  public Command cm_setElevatorState(String targetState){
+    return runOnce(() -> setElevatorState(targetState));
   }
 
   // method to set the position of the elevator
-  public void setElevatorPosition(double position) {
-    elevatorMotor.setPosition(position);
-  }
 
   public Command cm_elevatorMovement(double speed) {
-    return startEnd(() -> runElevatorMotor(speed), () -> stopElevatorMotor());
+    return startEnd(() -> runElevatorMotors(speed), () -> stopElevatorMotors());
   }
+
+  public double returnPID(){
+    return pidCalc;
+  }
+
 
   public void initSendable(SendableBuilder builder) {
     builder.addDoubleProperty(
-        "Left Climber Encoder Position",
-        () -> elevatorMotor.getPosition().getValueAsDouble(),
+        "Left Elevator Encoder Position",
+        () -> leftElevatorMotor.getPosition().getValueAsDouble(),
         null);
-    builder.addDoubleProperty("Elevator percent output", () -> elevatorMotor.get(), null);
+    builder.addDoubleProperty(
+        "Right Elevator Encoder Position",
+        () -> rightElevatorMotor.getPosition().getValueAsDouble(),
+        null);
+    builder.addDoubleProperty("Left Elevator percent output", () -> leftElevatorMotor.get(), null);
+    builder.addDoubleProperty("Right Elevator percent output", () -> rightElevatorMotor.get(), null);
+
+    builder.addDoubleProperty("PID Value", () -> returnPID(), null);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
   }
 }
