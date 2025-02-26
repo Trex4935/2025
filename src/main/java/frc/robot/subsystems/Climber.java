f2f1ff8a5d94c630a13c5a4cde273c0f702dd0ec
slashdasher <yoofiae@gmail.com>
2025-02-26 18:28:00 -0500

    converted colors from strings to objects

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -6,36 +6,36 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
-
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
-public final TalonFX climberMotor;
-private MotionMagicVoltage motionMagicVoltage = new MotionMagicVoltage(0).withSlot(1);
+  public final TalonFX climberMotor;
+  private MotionMagicVoltage motionMagicVoltage = new MotionMagicVoltage(0).withSlot(1);
 
   /** Creates a new Climber. */
   public Climber() {
     climberMotor = new TalonFX(7);
   }
 
+  public void moveClimberMotor(double speed) {
+    climberMotor.setControl(motionMagicVoltage.withPosition(speed));
+  }
 
- public void moveClimberMotor(double speed){
-  climberMotor.setControl(motionMagicVoltage.withPosition(speed));
- }
+  public void stopClimberMotor() {
+    climberMotor.stopMotor();
+  }
 
- public void stopClimberMotor(){
-  climberMotor.stopMotor();
- }
+  public Command climberMovement() {
+    return runEnd(() -> moveClimberMotor(0.5), () -> stopClimberMotor());
+  }
 
- public Command climberMovement(){
-  return runEnd(() -> moveClimberMotor(0.5), () -> stopClimberMotor());
- }
+  public void initSendable(SendableBuilder builder) {
+    builder.addDoubleProperty(
+        "Climber Encoder Pos", () -> climberMotor.getPosition().getValueAsDouble(), null);
+  }
 
- public void initSendable(SendableBuilder builder){
-  builder.addDoubleProperty("Climber Encoder Pos", () ->  climberMotor.getPosition().getValueAsDouble(), null);
- }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
