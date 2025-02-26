74799e048d21239290c7f34d3214e08929637826
slashdasher <yoofiae@gmail.com>
2025-02-26 18:47:44 -0500

    changed colors from strings to objects

--- src/main/java/frc/robot/extensions/StateMachine.java
+++ src/main/java/frc/robot/extensions/StateMachine.java
@@ -4,6 +4,8 @@
 
 package frc.robot.extensions;
 
+import edu.wpi.first.wpilibj.util.Color;
+
 /** Add your docs here. */
 public class StateMachine {
 
@@ -16,20 +18,20 @@ public class StateMachine {
    *     gravity feedforward.
    */
   public enum BotState {
-    DEFAULT(0, 0, "Red"), // field state, default state, no game pieces
-    INTAKECORAL(0, 0.2, "Orange"), // preparing to intake coral
-    INTAKEALGAE(0, 0, "Yellow"), // preparing to intake algae
-    STORAGE(0, 0, "Green"), // intaked, coral or algae in
-    REEF(0, -0.2, "Blue"), // scoring coral
-    PROCESSOR(0, 0, "Purple"), // scoring algae
-    CLIMB(0.3, 0, "Pink"), // climbing
-    EJECT(0.3, 0.2, "White"); // everything out
+    DEFAULT(0, 0, Color.kRed), // field state, default state, no game pieces
+    INTAKECORAL(0, 0.2, Color.kOrange), // preparing to intake coral
+    INTAKEALGAE(0, 0, Color.kYellow), // preparing to intake algae
+    STORAGE(0, 0, Color.kGreen), // intaked, coral or algae in
+    REEF(0, -0.2, Color.kBlue), // scoring coral
+    PROCESSOR(0, 0, Color.kPurple), // scoring algae
+    CLIMB(0.3, 0, Color.kPink), // climbing
+    EJECT(0.3, 0.2, Color.kWhite); // everything out
 
     public final double elevatorPosition;
     public final double coralIntakePosition;
-    public final String colorDisplay;
+    public final Color colorDisplay;
 
-    private BotState(double elevatorSpeed, double coralIntakeSpeed, String color) {
+    private BotState(double elevatorSpeed, double coralIntakeSpeed, Color color) {
       this.elevatorPosition = elevatorSpeed;
       this.coralIntakePosition = coralIntakeSpeed;
       this.colorDisplay = color;
