d630e9a9f02c0f23f687f2dd6cc04474c808b396
NotAVeryHappyPerson <rithviknadendla@gmail.com>
2025-03-04 18:09:46 -0500

    More code cleanup/refactoring

--- src/main/java/frc/robot/commands/cm_SetElevatorSpeed.java
+++ src/main/java/frc/robot/commands/cm_SetElevatorSpeed.java
@@ -5,7 +5,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.robot.subsystems.Elevator;
 
 /** An example command that uses an example subsystem. */
