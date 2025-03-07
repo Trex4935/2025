1a30f831805ca545e75a94d8edd3838c084c46e0
devito <admin@trex4935.com>
2025-03-06 19:48:14 -0500

    tweaking intake

--- src/main/java/frc/robot/commands/cm_FullSequence.java
+++ src/main/java/frc/robot/commands/cm_FullSequence.java
@@ -25,7 +25,7 @@ public class cm_FullSequence extends SequentialCommandGroup {
         StateMachine.setGlobalState(botState),
         leds.cm_setLedToColor(botState.colorDisplay),
         new cm_SetElevatorPosition(elevator).withTimeout(5),
-        new cm_SetCoralIntake(coralIntake).withTimeout(3),
+        new cm_SetCoralIntake(coralIntake),
 
         // Resets the state to default
         new cm_SetToDefault(elevator, leds));
