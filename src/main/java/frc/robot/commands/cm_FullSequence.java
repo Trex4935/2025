9292dcc8364b13e5203cc42cb4f1b65227b7261d
slashdasher <yoofiae@gmail.com>
2025-03-10 19:00:58 -0400

    Eject coral in full sequence

--- src/main/java/frc/robot/commands/cm_FullSequence.java
+++ src/main/java/frc/robot/commands/cm_FullSequence.java
@@ -27,7 +27,7 @@ public class cm_FullSequence extends SequentialCommandGroup {
         new cm_SetElevatorPosition(elevator)
             .withTimeout(5)
             .alongWith(new cm_SetPivotAngle(coralIntake).withTimeout(7)),
-        new cm_SetCoralIntake(coralIntake),
+        new cm_SetCoralEject(coralIntake),
 
         // Resets the state to default
         new cm_SetToDefault(elevator, leds));
