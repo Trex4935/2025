ad555d0308913e1950af73093be9a918f3a37af9
alecsis <josephddang00@gmail.com>
2025-03-22 08:30:39 -0400

    quick fix

--- src/main/java/frc/robot/commands/cm_ClimbSequence.java
+++ src/main/java/frc/robot/commands/cm_ClimbSequence.java
@@ -8,7 +8,7 @@ public class cm_ClimbSequence extends SequentialCommandGroup {
   public cm_ClimbSequence(double climberPosition, Climber climber) {
     addCommands(
         // Deploys solenoid
-        // climber.cm_solenoidToggle(),
+        climber.cm_solenoidToggle(),
 
         // Sets position of motor using motion magic, need to tune constants
         new cm_SetClimberPosition(climber, climberPosition).withTimeout(5));
