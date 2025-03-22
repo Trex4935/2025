07fba1d9bbe69d14d2b9490c99516a437d99f814
devito <admin@trex4935.com>
2025-03-22 13:27:44 -0400

    new branch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,6 @@ import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
-import com.pathplanner.lib.commands.PathPlannerAuto;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -117,6 +116,7 @@ public class RobotContainer {
     NamedCommands.registerCommand("L3", cmd_FullSequenceL3);
     NamedCommands.registerCommand("L4", cmd_FullSequenceL4);
     NamedCommands.registerCommand("Coral Intake", cmd_HumanIntake);
+    NamedCommands.registerCommand("AlgeeKill", cmd_AlgaeRemoval);
 
     // Determine which drivetrain we are using
     if (drivetrainDIO.get()) {
@@ -291,7 +291,7 @@ public class RobotContainer {
     // This method loads the auto when it is called, however, it is recommended
     // to first load your paths/autos when code starts, then return the
     // pre-loaded auto/path
-    return new PathPlannerAuto("R3 Two Piece");
-    // return autoChooser.getSelected();
+    // return new PathPlannerAuto("R3 Two Piece");
+    return autoChooser.getSelected();
   }
 }
