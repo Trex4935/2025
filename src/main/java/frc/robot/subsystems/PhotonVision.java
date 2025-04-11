8399bb8101881107485d842980534311b6c915ac
slashdasher <yoofiae@gmail.com>
2025-04-11 19:29:26 -0400

    added methods to get the best target and the latest result of photonvision camera

--- src/main/java/frc/robot/subsystems/PhotonVision.java
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -5,16 +5,33 @@
 package frc.robot.subsystems;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PhotonVision extends SubsystemBase {
   /** Creates a new PhotonVision. */
-  public PhotonVision() {
-    PhotonCamera camera;
+  PhotonCamera camera;
+  Transform2d getCamera;
+
+  public PhotonVision(String name, Transform2d getCameraPosition) {
     camera = new PhotonCamera(null);
+    getCamera = getCameraPosition;
 
   }
+
+  public void getLatestTarget() { // gets the latest result from PhotonVision
+    camera.getLatestResult();
+
+  }
+
+  public void getBestTarget(){ // gets the best target that the robot can go to
+    PhotonTrackedTarget target = camera.getBestTarget();
+  }
+
+
 
 
   @Override
