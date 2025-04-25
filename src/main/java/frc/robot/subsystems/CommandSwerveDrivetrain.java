8b590d3e28d10c9399a058be0a751a44750fa2b6
devito <admin@trex4935.com>
2025-04-25 17:29:31 -0400

    b

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -115,6 +115,9 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
   /* The SysId routine to test */
   private SysIdRoutine m_sysIdRoutineToApply = m_sysIdRoutineTranslation;
 
+  Alliance ally = DriverStation.getAlliance().orElse(Alliance.Blue);
+
+
   /**
    * Constructs a CTRE SwerveDrivetrain using the specified constants.
    *
